e3956807a6430ac8448714d8ce2197f66678bd1b
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-08-28 13:10:57 +0000

    Arquivos

--- 1-arquivos/maiusculas.py
+++ 1-arquivos/maiusculas.py
@@ -22,25 +22,29 @@ def main():
     #Abre um arquivo no modo de escrita.
     #O comando "with" garante que o arquivo será fechado automaticamente.
     #O comando "encoding='utf-8'" evita problemas com acentos no Windows, mas o Codespace é Linux.
+    #O camando "with" faz com que o arquivo seja fechado automaticamente.
     with open("meu_arquivo.txt","w",encoding='utf-8') as arquivo:  
-        #Para cada frase no modo de entrada, salva no arquivo com uma quebra de linha.
+        #Para cada frase coletada na lista, escreve no arquivo com uma quebra de linha.
         for frase in frases:
             arquivo.write(frase + "\n")
-    
 
     print("Arquivo original criado. Agora vamos manipular os dados.")
-    #Cria uma lista, "[]", para receber os dados originais capturados inicialmente.
+    #Cria uma outra lista, "[]", para receber os dados originais capturados inicialmente.
     dados_modificados = []
     #Abre o arquivo original no modo leitura.
     with open("meu_arquivo.txt", "r") as arquivo: #A palavra "arquivo" está dentro do escopo do "with"
         for linha in arquivo:
-            dados_modificados.append(linha.strip().upper()) #Retira os espaços, coloca em maiúscula e salva na lista.
+            dados_modificados.append(linha.strip().upper()) #Retira os espaços em branco e quebras de linha, 
+                                                            #converte para maiúscula e salva na lista
+                                                            #"dados_modificados".
 
-    #Sobrescreve os dados originais com os dados modificados.
+    #Sobrescreve os dados originais com os dados modificados,
+    #adicionando uma quebra de linha a cada frase.
     with open("meu_arquivo.txt", "w") as arquivo:
         for linha in dados_modificados:
             arquivo.write(linha + "\n")
     print("O arquivo foi sobrescrito com os dados modificados.")
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": #Verifica se o script está sendo executado diretamente,
+                           #não importado como módulo.
+    main() #Chama afunção main.
